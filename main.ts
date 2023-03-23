746c7939c39684f16d589c70c38cfc5bb9f77faf
sarianop <123103096+sarianop@users.noreply.github.com>
2023-03-23 08:03:28 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,35 @@
-let guess = 0
-let value = randint(1, 5)
-let secondValue = randint(1, 5)
-game.splash("Answer the question!")
-while (guess != value + secondValue) {
-    guess = parseFloat(game.askForString("What is the sum of " + value + " and " + secondValue + "?", 1))
+function equationAdd(value1min: number, value1max: number, value2min: number, value2max: number) {
+    let guess = 0
+    let value = randint(value1min, value1max)
+    let secondValue = randint(value2min, value2max)
+    game.splash("Answer the question!")
+    while (guess != value + secondValue) {
+        guess = parseFloat(game.askForString("What is the sum of " + value + " and " + secondValue + "?", 3))
+    }
+    game.splash("Correct!")
 }
-game.splash("Correct!")
-game.over(true)
 
+function equationMult(value1min: number, value1max: number, value2min: number, value2max: number) {
+    let guess = 0
+    let correct = true
+    let value = randint(value1min, value1max)
+    let secondValue = randint(value2min, value2max)
+    game.splash("Answer the question!")
+    guess = parseFloat(game.askForString("What is " + value + " multiplied by " + secondValue + "?", 4))
+    if (guess == value * secondValue) {
+        correct = true
+    } else {
+        correct = false
+    }
+    return correct
+}
+
+while (true) {
+let getNums = game.askForNumber("Type in a number",2)
+
+if (equationMult(getNums, getNums * 2, 7, 9) == true) {
+    game.splash("That is correct")
+} else {
+    game.splash("You're wrong stupid")
+}
+}
